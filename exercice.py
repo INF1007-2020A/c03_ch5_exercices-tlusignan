96b0ecf8b6ac97a18ae9b7da37379bd2993e1001
tlusignan <71023627+tlusignan@users.noreply.github.com>
2020-09-21 16:12:47 -0400

    Exercices complétés

--- exercice.py
+++ exercice.py
@@ -6,29 +6,56 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    num = float((input('Please input a number: ')))
+    if(num < 0):    
+        num = -num
+    return num
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-
-    return [""]
+    noms = []
+    for prefix in prefixes:
+        nom = prefix + suffixes
+        noms.append(nom)
+    return noms
 
 
 def prime_integer_summation() -> int:
-    return 0
+    n=1
+    num = 1
+    sumofprimes = 0
+    while n<=100:
+        isPrime = True
+        for n in range(2, num//2):
+            if (num%n) == 0:
+                isPrime=False
+
+        if isPrime:
+            sumofprimes+=num
+            n += 1
+            # print(f'{n} {num} {sumofprimes}')
+
+        num+=2
+    return sumofprimes
 
 
 def factorial(number: int) -> int:
-    return 0
+    fact=1
+    for n in range(1, number):
+        fact *= n
+    return fact
 
 
 def use_continue() -> None:
-    pass
+    for i in range(1, 11):
+        if i==5: continue
+        else: print(i)
+    return 0
 
 
 def main() -> None:
-    print(f"La valeur absolue du nombre est {convert_to_absolute()}")
+    # print(f"La valeur absolue du nombre est {convert_to_absolute()}")
 
     print(f"La liste des noms générés avec les préfixes est: {use_prefixes()}")
 
@@ -38,6 +65,7 @@ def main() -> None:
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
     
     print(f"L'affichage de la boucle est:")
+
     use_continue()
 
 
